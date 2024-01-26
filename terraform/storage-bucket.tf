9aaa157c3b1e68460f74d91709f75f617b0dc235
alkstmshkn <bar.s81@mail.ru>
2024-01-27 00:16:34 +0300

    hw7 и добавил недостающие файлы

---
+++ terraform/storage-bucket.tf
@@ -0,0 +1,29 @@
+
+terraform {
+/* required_version = ">= 1.0.0"
+  required_providers {
+    yandex    = {
+      source  = "yandex-cloud/yandex"
+      version = ">= 0.103.0"
+    }
+  }
+}
+*/
+
+  backend "s3" {
+    endpoint = "storage.yandexcloud.net"
+    bucket   = "xxxxxxxxxxxxxxx"
+    region   = "ru-central1-a"
+    key      = "stage.tfstate"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+}
+
+provider "yandex" {
+  service_account_key_file = var.service_account_key_file
+  cloud_id                 = var.cloud_id
+  folder_id                = var.folder_id
+  zone                     = var.zone
+}
